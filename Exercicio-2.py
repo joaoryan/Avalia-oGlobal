ef1afb867130b298c59f4ab7517ed544aafbc0e1
joaoryan <sjoaoryan@gmail.com>
2022-10-31 21:56:39 -0300

    feat: questão 2

--- Exercicio-2.py
+++ Exercicio-2.py
@@ -3,9 +3,8 @@ from sympy import Symbol, Derivative
 matricula = 9594
 c = matricula % 10
 
-
 def eqPosicao(t):
-    return (-t**3) / 3 + 2*t**2 - c
+    return (t ** (1/4)) + (1/t**2) -3 * c
 
 
 t = Symbol('t')
@@ -13,7 +12,7 @@ eqVelocidade = Derivative(eqPosicao(t), t).doit()
 
 print('\nv(t) = ', eqVelocidade, '\n')
 
-velocidade_3 = Derivative(eqPosicao(t), t).doit().subs({t: 3})
+velocidade_3 = Derivative(eqPosicao(t), t).doit().subs({t: 7})
 
 print('\nv(3) = ', velocidade_3, '\n')
 
@@ -21,6 +20,6 @@ eqAceleracao = Derivative(eqVelocidade, t).doit()
 
 print('\na(t) = ', eqAceleracao, '\n')
 
-aceleracao_5 = Derivative(eqVelocidade, t).doit().subs({t: 5})
+aceleracao_5 = Derivative(eqVelocidade, t).doit().subs({t: 2})
 
 print('\na(5) = ', aceleracao_5, '\n')
